442ce099979bec00b57ee44a99dcb37358cd3d0a
kha845 <dkhadidiatou75@gmail.com>
2022-08-10 19:50:45 +0000

    reset your password

--- resources/views/mail/reset_password.blade.php
+++ resources/views/mail/reset_password.blade.php
@@ -4,10 +4,10 @@
     We received a request to change password.
     If you are not initiator of this request,please let us know for the security of your account.
     <br>If you are the initiator click to link below to reset your password<br>
-    or by clicking the following link: <br>
-    <a href="#" target="_blank">confirm your account </a>
+    or by clicking the following link below to reset your password <br>
+    <a href="#" target="_blank">Reset password</a>
 </p>
 <p>
-    ColobaneOnline team.
+    ColobaneOnline security team.
 
 </p>
