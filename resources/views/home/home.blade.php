b25dfa2892c80c45bdc587dc79af6f1a09d1ffca
kha845 <dkhadidiatou75@gmail.com>
2022-08-11 00:35:27 +0000

    change style

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -2,5 +2,55 @@
 @section('title','Home')
 @section('content')
 @section('content')
-<h1>Home</h1>
+<div align-center>
+    <h1>Bienvenue  chez Almaktum<h1>
+</div>
+<!-- Gallery -->
+<div class="row">
+    <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+      <img
+        src="images/image1.jpg"
+        class="w-100 shadow-1-strong rounded mb-4"
+        alt="Boat on Calm Water"
+      />
+
+      <img
+        src="images/image2.jpg"
+        class="w-100 shadow-1-strong rounded mb-4"
+        alt="Wintry Mountain Landscape"
+      />
+    </div>
+
+    <div class="col-lg-4 mb-4 mb-lg-0">
+      <img
+        src="images/image3.jpg"
+        class="w-100 shadow-1-strong rounded mb-4"
+        alt="Mountains in the Clouds"
+      />
+
+      <img
+        src="images/image4.jpg"
+        class="w-100 shadow-1-strong rounded mb-4"
+        alt="Boat on Calm Water"
+      />
+    </div>
+
+    <div class="col-lg-4 mb-4 mb-lg-0">
+      <img
+        src="images/image5.jpg"
+        class="w-100 shadow-1-strong rounded mb-4"
+        alt="Waves at Sea"
+      />
+
+      <img
+        src="images/image6.jpg"
+        class="w-100 shadow-1-strong rounded mb-4"
+        alt="Yosemite National Park"
+      />
+    </div>
+  </div>
+  <!-- Gallery -->
+
+
+
 @endsection
